9d0fb75e0e2b62dd14200b935aea9919f8774192
“Patricio <carcani.patricio@gmail.com>
2020-06-08 18:32:51 -0300

    Anadida clase mensaje - Sistema funcional en este punto

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,8 @@ export class ChatComponent implements OnInit {
 
   private client: Client;
   conectado: boolean = false;
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
@@ -23,6 +26,13 @@ export class ChatComponent implements OnInit {
     this.client.onConnect = (frame) => {
       console.log('Conectados: ' + this.client.connected + ' : ' + frame);
       this.conectado=true;
+      //suscribirse y escuchar a evento chat
+      this.client.subscribe('/chat/mensaje', e => {
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
     }
 
     this.client.onDisconnect = (frame) => {
@@ -36,8 +46,15 @@ export class ChatComponent implements OnInit {
     this.client.activate();
   }
 
-  desconectar(): void{
+  desconectar(): void {
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    // message mapping
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    // texto reiniciado
+    this.mensaje.texto = '';
   }
 
 }
---
+++ src/app/chat/models/mensaje.ts
@@ -0,0 +1,6 @@
+export class Mensaje {
+
+    texto: string = '';
+    fecha: Date;
+
+}
